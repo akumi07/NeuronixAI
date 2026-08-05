74b213ebe064065d5c246ce98af9a0a0b9353863
akumi07 <akash9338144396@gmail.com>
2026-08-05 23:06:53 +0530

    folder structure updated

---
+++ src/main/java/com/neuronix/NeuronixBackendApplication.java
@@ -0,0 +1,14 @@
+package com.neuronix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuronixBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NeuronixBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neuronix/NeuronixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neuronix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuronixBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
